9bf1babc566419ae22fda51261eb276d75545508
AguStarlip <lipchak09gmail.com>
2020-08-17 20:00:09 -0300

    Restserver-user

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,40 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            memsaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
+        if (err) throw err;
 
-});
+        console.log('Base de datos ONLINE');
 
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
     });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
-
 
 app.listen(process.env.PORT, () => {
     console.log('Esuchando el puerto: ', process.env.PORT);
