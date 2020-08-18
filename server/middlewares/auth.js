33881641d5257f244c64e57395df2cb988b8d594
AguStarlip <lipchak09gmail.com>
2020-08-18 18:49:18 -0300

    added-normal-login

---
+++ server/middlewares/auth.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken');
+
+//================
+// Verificar Token
+//================
+
+let verificaToken = (req, res, next) => {
+
+    let token = req.get('Authorization');
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+
+
+
+};
+
+//================
+// Verificar AdminRole
+//================
+
+let verificaAdmin_Role = (req, res, next) => {
+
+    let usuario_role = req.usuario.role;
+
+    if (usuario_role !== 'ADMIN_ROLE') {
+        return res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es administrador'
+            }
+        });
+    } else {
+        next();
+    }
+
+}
+
+module.exports = {
+    verificaToken,
+    verificaAdmin_Role
+}
