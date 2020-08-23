8bbb03b552f9eeaa4fc5f5665103cf2ac8d28e3a
AguStarlip <lipchak09gmail.com>
2020-08-23 17:28:33 -0300

    added file-upload-view

---
+++ server/routes/imagenes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { verificaTokenImg } = require('../middlewares/auth');
+
+let app = express();
+
+app.get('/imagen/:tipo/:img', verificaTokenImg, (req, res) => {
+
+    let tipo = req.params.tipo;
+    let img = req.params.img;
+
+    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${img}`);
+
+    if (fs.existsSync(pathImagen)) {
+        res.sendFile(pathImagen);
+    } else {
+        let notImagePath = path.resolve(__dirname, '../assets/no-image.jpg');
+        res.sendFile(notImagePath);
+    }
+
+});
+
+
+
+
+
+module.exports = app;
